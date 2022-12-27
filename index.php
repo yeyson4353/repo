0c70f24e9ba9e40bbc2b8ef500ee05972c5770b3
yeyson4353 <121024576+yeyson4353@users.noreply.github.com>
2022-12-27 21:11:58 +0100

    leyenda

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form>
+        <fieldset>
+            <legend>Equip 1</legend>not
+            <label for="name">Nom:</label><br>
+            <input type="text" id="name" name="name"><br>
+            <label for="address">Dirección:</label><br>
+            <input type="text" id="address" name="address"><br>
+            <label for="phone">Número de teléfono:</label><br>
+            <input type="text" id="phone" name="phone"><br>
+            <label for="color-picker">Elige un color:</label><br>
+            <input type="color" id="color-picker" name="color-picker">
+        </fieldset>
+</form> 
+    
+</body>
+</html>
